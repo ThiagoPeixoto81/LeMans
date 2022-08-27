3878f2da25a22a0901c35db488f132d8f24d23c3
ThiagoPeixoto81 <thiagopeixoto81xd@gmail.com>
2022-08-27 18:55:53 -0300

    Usando JSON para guardar os dados

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,3 @@
-// Pegar as informações 
-import * as win_info from './winners.js';
-
-var winners = win_info.GetWinners();
-
 //Pega os elementos
 let yearButton = document.querySelectorAll(".year-btn");
 let show = document.querySelector(".info-section")
@@ -13,26 +8,38 @@ let team_name = document.querySelector(".team-name")
 let drivers_name = document.querySelector(".drivers-name")
 let logo = document.querySelector(".logo-img")
 
-//Evento de ativação da Div
-for (let i in yearButton) {
-    yearButton[i].addEventListener("click", () => {
-        for (let pos in winners) {
-            if (winners[pos].year == yearButton[i].dataset.year) {
-                var car = winners[pos];
-            }
+//Pegando o JSON
+fetch("/scripts/winners.json")
+    .then(res => res.json())
+    .then(lemans => {
+        // Ativação da div com as informações dos carros
+        var lastcliked = 0;
+        for (let i in yearButton) {
+            yearButton[i].addEventListener("click", () => {
+                if (yearButton[i].dataset.year == lastcliked) {
+                    show.classList.remove("on")
+                    lastcliked = 0;
+
+                } else {
+
+                    for (let pos in lemans.winners) {
+                        if (lemans.winners[pos].year == yearButton[i].dataset.year) {
+
+                            year.innerText = `${lemans.winners[pos].year}`;
+                            car_name.innerHTML = `<strong>CARRO - </strong>${lemans.winners[pos].name.toUpperCase()}`;
+                            team_name.innerHTML = `<strong>TIME - </strong>${lemans.winners[pos].constructors.toUpperCase()}`;
+                            drivers_name.innerHTML = `<strong>PILOTOS:</strong><br>${lemans.winners[pos].drivers.toUpperCase()}`;
+
+                            car_image.src = `${lemans.winners[pos].img_src}`;
+                            logo.src = `${lemans.winners[pos].con_src}`;
+
+                            show.classList.add("on");
+                            lastcliked = yearButton[i].dataset.year;
+                        }
+                    }
+
+                }
+
+            })
         }
-
-        year.innerText = `${car.year}`
-        car_name.innerHTML = `<strong>CARRO - </strong>${car.name.toUpperCase()}`
-        team_name.innerHTML = `<strong>TIME - </strong>${car.constructors.toUpperCase()}`
-        drivers_name.innerHTML = `<strong>PILOTOS:</strong><br>${car.drivers.toUpperCase()}`
-
-        car_image.src = `${car.img_src}`
-        logo.src = `${car.con_src}`
-
-
-        show.classList.add("on")
-        window.scrollTo(0, 0)
-
     })
-}
